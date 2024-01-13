02963a5304d1a2db80fda4dae714066d447bca4b
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2024-01-14 04:40:17 +0530

    Fix done on line 6

--- rise.py
+++ rise.py
@@ -3,7 +3,8 @@ print("Let's get started")
 x = "hello"
 
 #if condition returns True, then nothing happens:
-assert x == "hello
+assert x == "hello"
+print("Rise & shine")
 
-#if condition returns False, AssertionError is raised:
-# assert x == "goodbye"
+# if condition returns False, AssertionError is raised:
+assert x == "goodbye"
