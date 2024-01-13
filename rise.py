937755b370fe70600095255c69b634b8bb04ce27
IndraSingh1992 <Indrabhushan.singh@hotmail.com>
2024-01-14 03:51:27 +0530

    added  2 lines of code

--- rise.py
+++ rise.py
@@ -1,2 +1,9 @@
 print("Rise & shine")
 print("Let's get started")
+x = "hello"
+
+#if condition returns True, then nothing happens:
+assert x == "hell"
+
+#if condition returns False, AssertionError is raised:
+# assert x == "goodbye"
